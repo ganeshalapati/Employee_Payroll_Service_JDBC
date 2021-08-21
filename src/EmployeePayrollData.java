44f46f6435d3ae94c28bf46d4e0525c834f9b29c
ganeshalapati <alapatiganesh31@gmail.com>
2021-08-21 05:33:35 +0530

    Added a method to retrive data

---
+++ src/EmployeePayrollData.java
@@ -0,0 +1,15 @@
+
+public class EmployeePayrollData {
+	 public int employee_id;
+	    public String name;
+	    public double salary;
+	    public LocalDate start;
+
+	    public EmployeePayrollData(int id, String name, double salary, LocalDate start) {
+	        this.employee_id = id;
+	        this.name = name;
+	        this.salary = salary;
+	        this.start = start;
+	    }
+
+}
---
+++ src/EmployeePayrollService.java
@@ -0,0 +1,21 @@
+
+public class EmployeePayrollService {
+	EmployeePayrollDBService dbService = new EmployeePayrollDBService();
+
+    public enum IOService{
+        DB_IO;
+    }
+
+    private List<EmployeePayrollData> employeePayrollData;
+
+    public EmployeePayrollService() {
+    }
+
+    public List<EmployeePayrollData> readEmployeePayrollData(IOService ioService) {
+        if(ioService.equals(IOService.DB_IO))
+            this.employeePayrollData = dbService.readData();
+        return this.employeePayrollData;
+    }
+	
+
+}
